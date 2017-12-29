ea31fde2f348d931e97b80fd1914bf04d45922aa
Agnese Salutari <agneses92@hotmail.it>
2017-12-29 16:25:28 +0100

    Create senseNode.py

---
+++ src/senseNode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+
+def odomCallback(data):
+  x = msg.pose.pose.position.x
+  y = msg.pose.pose.position.y
+  print('Odometry Data: x=' + str(x) + ' y=' + str(y))
+
+def main():
+  rospy.init_node('sense')
+  rospy.Subscriber("/odom", Odometry, odomCallback)
+  rospy.spin()
+  
+if __name__ == 'main':
+main()
