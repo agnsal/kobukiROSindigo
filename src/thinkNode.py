4108079ffd3d4441b47ab160ef53c9921de35568
Agnese Salutari <agneses92@hotmail.it>
2018-01-03 16:07:14 +0100

    Update thinkNode.py

--- src/thinkNode.py
+++ src/thinkNode.py
@@ -3,4 +3,25 @@
 import rospy
 import math
 import time
+from pyswip import Prolog
 from kobuki_project.msg import Status
+
+publisher_status = rospy.Publisher('kobuki_velocity', Twist, queue_size=1)
+kobukiStatus = Status() # The updated status of the robot
+
+def decisionCallback(kobukiStatus):
+    west = kobukiStatus.bumperW
+    north = kobukiStatus.bumperN
+    est = kobukiStatus.bumperE
+    rospy.loginfo('west: {}, north: {}, est: {}'.format(west, north, est))
+
+def think():
+    rospy.init_node('act')
+    rospy.Subscriber("/kobuki_status", Status, decisionCallback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        think()
+    except rospy.ROSInterruptException:
+pass
