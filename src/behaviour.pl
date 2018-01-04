8942b26b0bf26f2955036d46d031e4c95077f21b
Agnese Salutari <agneses92@hotmail.it>
2018-01-04 12:34:49 +0100

    Create behaviour.pl

---
+++ src/behaviour.pl
@@ -0,0 +1,14 @@
+hasValue('bumperW', W) :- perceptionNorth([['bumperW', W], _, _]).
+hasValue('bumperN', N) :- perceptionNorth([_, ['bumperN', N], _]).
+hasValue('bumperE', E) :- perceptionNorth([_, _, ['bumperE', E]]).
+
+takeDecision('North') :-
+    hasValue('bumperN', 'False'), !.
+
+takeDecision('West') :-
+    hasValue('bumperW', 'False'), !.
+
+takeDecision('Est') :-
+    hasValue8('bumperE', 'False'), !.
+  
+takeDecision('South&Turn').
