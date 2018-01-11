6da1724c0c0a92d7929c730917a5b6ef9ec31326
Agnese Salutari <agneses92@hotmail.it>
2018-01-11 17:17:52 +0100

    Update senseNode.py
    Camera callback added

--- src/senseNode.py
+++ src/senseNode.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
+import cv2, cv_bridge
 from nav_msgs.msg import Odometry
 from kobuki_msgs.msg import BumperEvent
 from kobuki_msgs.msg import PowerSystemEvent
 from kobukiROSindigo.msg import Status
+from sensor_msgs.msg import Image
 
 pubKobukiStatus = rospy.Publisher('kobuki_status', Status, queue_size=1)
 kobukiStatus = Status()  # The updated status of the robot
+bridge = cv_bridge.CvBridge()
+cv2.namedWindow("robotWiew", 1)
+
+def cameraCallback(data):
+    sUnixTimestamp = int(time.time())  # Timestamp in seconds
+	image = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
+	cv2.imshow("robotWiew", image)
+	cv2.waitKey(3)  # Display a frame for 3 ms
+    rospy.loginfo('cameraTimestamp: {}'.format(sUnixTimestamp))
 
 def odometryCallback(data):
     kobukiStatus.odometryX = data.pose.pose.position.x
@@ -54,12 +65,13 @@ def powerCallback(data):
         rospy.loginfo("WARN: Unexpected power system event: %d"%(data.event))
     pubKobukiStatus.publish(kobukiStatus)
     rospy.loginfo('power: {}'.format(kobukiStatus.power))
-
+    
 def sense():
     rospy.init_node('sense')
     rospy.Subscriber("/odom", Odometry, odometryCallback)
     rospy.Subscriber("/mobile_base/events/power_system", PowerSystemEvent, powerCallback)
     rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, bumperCallback)
+    rospy.Subscriber("camera/rgb/image_raw", Image, cameraCallback)
     rospy.spin()
   
 if __name__ == '__main__':
